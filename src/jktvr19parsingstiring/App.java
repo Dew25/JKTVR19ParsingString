ce2d4f01883b6c495fcd0cfb81e6dbdc38ed26f9
Juri Melnikov <Juri.Melnikov@ivkhk.ee>
2020-09-25 16:00:29 +0300

    Решены все заявленные задачи

--- src/jktvr19parsingstiring/App.java
+++ src/jktvr19parsingstiring/App.java
@@ -12,6 +12,8 @@ import java.util.Scanner;
  * @author Melnikov
  */
 class App {
+    private Scanner scanner = new Scanner(System.in);
+    
     public void run(){
         System.out.println("--- Работа со строкой ---");
         System.out.println("---------------------------------------------");
@@ -22,8 +24,7 @@ class App {
         System.out.println("4. удалить все теги из html строки");
         System.out.println("5. удалить указанный тег из html строки");
         System.out.println("---------------------------------------------");
-        System.out.println("Введите исходную строку: ");
-        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите исходную строку (latin): ");
         String str = scanner.nextLine();
         System.out.println("---------------------------------------------");
         System.out.println("Выберите номер задачи: ");
@@ -35,18 +36,26 @@ class App {
                 break;
             case "2":
                 System.out.println("Вы выбрали: 2. найти указанную строку в исходной строке");
-                
+                int n = this.searchSubString(str);
+                if(n >= 0){
+                    System.out.println("Срока есть в позиции: " + n);
+                }else{
+                    System.out.println("Сроки нет");
+                }
                 break;
             case "3":
                 System.out.println("Вы выбрали: 3. заменить искомую строку на указанную");
-                
+                System.out.println("Результат: " + this.replaceString(str));
                 break;
             case "4":
                 System.out.println("Вы выбрали: 4. удалить все теги из html строки");
+                //используйте строку <!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Document</title></head><body>This is a text</body></html>
+                System.out.println("Результат: " + this.deleteTegsFromHtml(str));
                 
                 break;
             case "5":
                 System.out.println("Вы выбрали: 5. удалить указанный тег из html строки");
+                System.out.println("Результат: " + this.deleteOneTegFromHtml(str));
                 
                 break;
             default:
@@ -60,5 +69,51 @@ class App {
         }
         String[] arrStr = str.split(" ");
         return arrStr.length;
+    }
+    private int searchSubString(String str){
+        System.out.println("Введите искомую строку (latin): ");
+        String subStr = scanner.nextLine();
+        int n = str.toLowerCase().indexOf(subStr.toLowerCase());
+        return n;
+    }
+
+    private String replaceString(String str) {
+        System.out.println("Введите искомую строку (latin): ");
+        String subStr = scanner.nextLine();
+        System.out.println("На что заменить (latin): ");
+        String replaceStr = scanner.nextLine();
+        StringBuilder sb = new StringBuilder(str);
+        int firstIndex = str.indexOf(subStr);
+        int endIndex = firstIndex + subStr.length();
+        sb.replace(firstIndex, endIndex, replaceStr);
+        return sb.toString();
+    }
+
+    private String deleteTegsFromHtml(String str) {
+        StringBuilder sb = new StringBuilder(str);
+        while(sb.indexOf("<") >= 0){
+            int indexOpenTag = sb.indexOf("<");
+            int indexCloseTag = sb.indexOf(">");
+            sb.replace(indexOpenTag, indexCloseTag+1, " ");
+        }  
+        return sb.toString();
+    }
+
+    private String deleteOneTegFromHtml(String str) {
+        System.out.println("Укажите какой тег удалить (без угловых скобок): ");
+        String tag = scanner.nextLine();
+        StringBuilder sb = new StringBuilder(str);
+        String tagBegin = "<"+tag;
+        String tagEnd = "</"+tag;
+        while(sb.indexOf(tagBegin) >= 0){
+            int indexOpenTag = sb.indexOf(tagBegin);
+            int indexOpenTagEnd = sb.indexOf(">",indexOpenTag);
+           // int indexCloseTag = indexOpenTag + indexOpenTagClose+1;
+            sb.replace(indexOpenTag, indexOpenTagEnd+1, "");
+            int indexCloseTag = sb.indexOf(tagEnd);
+            int indexCloseTagEnd = sb.indexOf(">",indexCloseTag);
+            sb.replace(indexCloseTag, indexCloseTagEnd+1, "");
+        }  
+        return sb.toString();
     }
 }
