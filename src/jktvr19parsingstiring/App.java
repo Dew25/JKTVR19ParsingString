ab027852a5bbcae5d2441b313346ecc6981bb9e9
Juri Melnikov <Juri.Melnikov@ivkhk.ee>
2020-10-01 14:41:27 +0300

    Реализован класс App с интерфейсом управления программой

--- src/jktvr19parsingstiring/App.java
+++ src/jktvr19parsingstiring/App.java
@@ -17,50 +17,66 @@ class App {
     public void run(){
         System.out.println("--- Работа со строкой ---");
         System.out.println("---------------------------------------------");
-        System.out.println("Список задач: ");
-        System.out.println("1. посчитать количество слов в строке");
-        System.out.println("2. найти указанную строку в исходной строке");
-        System.out.println("3. заменить искомую строку на указанную");
-        System.out.println("4. удалить все теги из html строки");
-        System.out.println("5. удалить указанный тег из html строки");
-        System.out.println("---------------------------------------------");
-        System.out.println("Введите исходную строку (latin): ");
-        String str = scanner.nextLine();
-        System.out.println("---------------------------------------------");
-        System.out.println("Выберите номер задачи: ");
-        String task = scanner.nextLine();
-        switch (task) {
-            case "1":
-                System.out.println("Вы выбрали: 1. посчитать количество слов в строке");
-                System.out.println("В строке слов: " + this.getCountWords(str));
-                break;
-            case "2":
-                System.out.println("Вы выбрали: 2. найти указанную строку в исходной строке");
-                int n = this.searchSubString(str);
-                if(n >= 0){
-                    System.out.println("Срока есть в позиции: " + n);
-                }else{
-                    System.out.println("Сроки нет");
-                }
-                break;
-            case "3":
-                System.out.println("Вы выбрали: 3. заменить искомую строку на указанную");
-                System.out.println("Результат: " + this.replaceString(str));
-                break;
-            case "4":
-                System.out.println("Вы выбрали: 4. удалить все теги из html строки");
-                //используйте строку <!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Document</title></head><body>This is a text</body></html>
-                System.out.println("Результат: " + this.deleteTegsFromHtml(str));
-                
-                break;
-            case "5":
-                System.out.println("Вы выбрали: 5. удалить указанный тег из html строки");
-                System.out.println("Результат: " + this.deleteOneTegFromHtml(str));
-                
-                break;
-            default:
-                System.out.println("Нет такой задачи");
-        }
+        boolean repeat = true;
+        do{
+            System.out.println("Список задач: ");
+            System.out.println("0. Закончить программу");
+            System.out.println("1. посчитать количество слов в строке");
+            System.out.println("2. найти указанную строку в исходной строке");
+            System.out.println("3. заменить искомую строку на указанную");
+            System.out.println("4. удалить все теги из html строки");
+            System.out.println("5. удалить указанный тег из html строки");
+            System.out.println("---------------------------------------------");
+            System.out.println("---------------------------------------------");
+            System.out.println("Выберите номер задачи: ");
+            String task = scanner.nextLine();
+            switch (task) {
+                case "0":
+                    System.out.println("--- конец программы ---");
+                    repeat = false;
+                    break;
+                case "1":
+                    System.out.println("Вы выбрали: 1. посчитать количество слов в строке");
+                    System.out.println("Введите исходную строку (latin): ");
+                    String str = scanner.nextLine();
+                    System.out.println("В строке слов: " + this.getCountWords(str));
+                    break;
+                case "2":
+                    System.out.println("Вы выбрали: 2. найти указанную строку в исходной строке");
+                    System.out.println("Введите исходную строку (latin): ");
+                    str = scanner.nextLine();
+                    int n = this.searchSubString(str);
+                    if(n >= 0){
+                        System.out.println("Срока есть в позиции: " + n);
+                    }else{
+                        System.out.println("Сроки нет");
+                    }
+                    break;
+                case "3":
+                    System.out.println("Вы выбрали: 3. заменить искомую строку на указанную");
+                    System.out.println("Введите исходную строку (latin): ");
+                    str = scanner.nextLine();
+                    System.out.println("Результат: " + this.replaceString(str));
+                    break;
+                case "4":
+                    System.out.println("Вы выбрали: 4. удалить все теги из html строки");
+                    //используйте строку <!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Document</title></head><body>This is a text</body></html>
+                    System.out.println("Введите исходную строку (latin): ");
+                    str = scanner.nextLine();
+                    System.out.println("Результат: " + this.deleteTegsFromHtml(str));
+
+                    break;
+                case "5":
+                    System.out.println("Вы выбрали: 5. удалить указанный тег из html строки");
+                    System.out.println("Введите исходную строку (latin): ");
+                    str = scanner.nextLine();
+                    System.out.println("Результат: " + this.deleteOneTegFromHtml(str));
+
+                    break;
+                default:
+                    System.out.println("Нет такой задачи");
+            }
+        }while(repeat);
         
     }
     private int getCountWords(String str){
